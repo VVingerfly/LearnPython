82c3d5b8ffb0d38175ec7992340be9b30812173e
Wei Li <davidlee2776@gmail.com>
2017-01-06 13:52:02 +0800

    111

--- 8.RegularExpression/group.py
+++ 8.RegularExpression/group.py
@@ -1,7 +1,24 @@
 """
-C
+A group can be created by surrounding part of a regular expression with parentheses.
+This means that a group can be given as an argument to metacharacters such as * and ?.
 """
 import re
 
 print("_________________________ Test 1 _________________________")
+pattern = r"egg(spam)*"
 
+if re.match(pattern, "egg"):
+    print("match 1")
+
+if re.match(pattern, "eggspamspamspamegg"):
+    print("match 2")
+
+if re.match(pattern, "spam"):
+    print("match 3")
+
+if re.match(pattern, "egg2"):
+    print("match 4")
+
+
+print("_________________________ Test 2 _________________________")
+pattern = r"a"
